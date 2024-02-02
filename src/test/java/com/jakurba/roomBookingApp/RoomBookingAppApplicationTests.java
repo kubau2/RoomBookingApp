bd2706ba4b234422fad163253874263a1decab93
Jakub Urban <jakub.a.urban@capgemini.com>
2024-02-02 12:55:00 +0100

    Initial commit

---
+++ src/test/java/com/jakurba/roomBookingApp/RoomBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakurba.roomBookingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
