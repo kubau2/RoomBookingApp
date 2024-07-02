8e8d68b23ad71d30425c784ffc6c978e926ff66d
dzejju <dzejju@DESKTOP-BPI3F7B>
2024-07-02 11:19:27 +0200

    Creating reservation tests

--- src/test/java/com/jakurba/roomBookingApp/ReservationMvcTests.java
+++ src/test/java/com/jakurba/roomBookingApp/ReservationMvcTests.java
@@ -1,21 +1,14 @@
 package com.jakurba.roomBookingApp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.jakurba.roomBookingApp.model.Employee;
-import com.jakurba.roomBookingApp.model.Reservation;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,25 +25,73 @@ class ReservationMvcTests {
     //Those tests work based on the preloaded data.sql
 
     @Test
-    void findEmployeeByIdIs404() throws Exception {
-        Employee emp = new Employee(2L, "test@email.mail", "name", "surname");
-
-        String requestBody = objectMapper.writeValueAsString(emp);
-
-        mockMvc.perform(get("/api/employees/findById").contentType("application/json").content(requestBody)).andExpect(status().isNotFound()).andDo(print());
+    void createReservationIsOk() throws Exception {
+        String requestBody = """
+                {
+                    "room_Id": 1,
+                    "employee":{
+                        "id":1
+                    },
+                    "room":{
+                        "id":1
+                    },
+                    "reservationStart": "2024-02-05T08:25:43.511Z",
+                    "reservationEnd": "2024-02-05T12:25:43.511Z"
+                }""";
+        String response = mockMvc.perform(post("/api/reservation/new").contentType("application/json").content(requestBody)).andReturn().getResponse().getContentAsString();
+        Assertions.assertTrue(response.contains("Blue"));
+        Assertions.assertTrue(response.contains("john.williams@test.com"));
+        Assertions.assertTrue(response.contains("2024-02-05T12:25:43.511"));
     }
 
     @Test
-    void listAllEmployees() throws Exception {
-        MockHttpServletResponse response = mockMvc.perform(get("/api/employees").contentType("application/json")).andReturn().getResponse();
-        String responseContent = response.getContentAsString();
-        List<Employee> employeeList = new ArrayList<>();
-        Gson g = new Gson();
-        String[] responseSplitToArray = responseContent.split("\\[");
-        for (int i = 1; i < responseSplitToArray.length; i++) {
-            String employeeEntry = responseSplitToArray[i].replace("]", "");
-            employeeList.add(g.fromJson(employeeEntry, Employee.class));
-        }
-        assertEquals("john.williams@test.com", employeeList.getFirst().getEmail());
+    void createReservationIs400() throws Exception {
+        String requestBody = """
+                {
+                    "room_Id": 1,
+                    "employee":{
+                        "id": InvalidID
+                    },
+                    "room":{
+                        "id":1
+                    },
+                    "reservationStart": "2024-02-05T08:25:43.511Z",
+                    "reservationEnd": "2024-02-05T12:25:43.511Z"
+                }""";
+        mockMvc.perform(post("/api/reservation/new").contentType("application/json").content(requestBody)).andExpect(status().isBadRequest()).andDo(print());
     }
+
+    @Test
+    void listAllReservations() throws Exception {
+        //TODO: Mock reservation
+        createReservationIsOk();
+        MockHttpServletResponse response = mockMvc.perform(get("/api/reservations").contentType("application/json")).andReturn().getResponse();
+        String responseContent = response.getContentAsString();
+        Assertions.assertTrue(responseContent.contains("Blue"));
+    }
+
+    @Test
+    void deleteReservation() throws Exception {
+        //TODO: Mock reservation
+        createReservationIsOk();
+        String requestBody = """
+                {
+                    "reservationId": 1,
+                    "employeeId": 1
+                }""";
+        MockHttpServletResponse response = mockMvc.perform(delete("/api/reservation").contentType("application/json").content(requestBody)).andReturn().getResponse();
+        String responseContent = response.getContentAsString();
+        Assertions.assertTrue(Boolean.parseBoolean(responseContent));
+    }
+
+    @Test
+    void deleteReservationNotFound() throws Exception {
+        String requestBody = """
+                {
+                    "reservationId": 1,
+                    "employeeId": 1
+                }""";
+       mockMvc.perform(delete("/api/reservation").contentType("application/json").content(requestBody)).andExpect(status().isNotFound()).andDo(print());
+    }
+
 }
